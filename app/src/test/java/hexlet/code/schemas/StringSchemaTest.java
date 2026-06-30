f1503b0d6c42369e45fdd0cc54e4cb62458b249e
NeoEmo <155008342+NeoEmo@users.noreply.github.com>
2026-06-30 21:13:56 +0300

    Create StringSchemaTest.java

---
+++ app/src/test/java/hexlet/code/schemas/StringSchemaTest.java
@@ -0,0 +1,35 @@
+package hexlet.code.schemas;
+
+
+import hexlet.code.Validator;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+
+public class StringSchemaTest {
+
+    @Test
+    public void stringSchemaTest() {
+        var v = new Validator();
+        var schema = v.string();
+
+        assertTrue(schema.isValid(""));
+        assertTrue(schema.isValid(null));
+
+        schema.required();
+
+        assertFalse(schema.isValid(""));
+        assertFalse(schema.isValid(null));
+        assertTrue(schema.isValid("what does the fox say"));
+        assertTrue(schema.isValid("hexlet"));
+
+        assertTrue(schema.contains("wh").isValid("what does the fox say"));
+        assertTrue(schema.contains("what").isValid("what does the fox say"));
+        assertFalse(schema.contains("Ni ni ni ni ni ni").isValid("what does the fox say"));
+        assertFalse(schema.isValid("what does the fox say"));
+
+        var schema1 = v.string();
+        assertTrue(schema1.minLength(10).minLength(4).isValid("Hexlet"));
+    }
+}
