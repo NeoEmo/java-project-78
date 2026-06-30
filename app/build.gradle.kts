a897a375165dfc3bc28bab3ae4a11cbee3d81af6
NeoEmo <155008342+NeoEmo@users.noreply.github.com>
2026-06-30 17:25:31 +0300

    create "Matryoshka"-style project

---
+++ app/build.gradle.kts
@@ -0,0 +1,56 @@
+plugins {
+    id("checkstyle")
+    id("application")
+    id("org.sonarqube") version "7.3.1.8318"
+    id("jacoco")
+}
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+application {
+    mainClass = "hexlet.code.App"
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+
+    testImplementation("org.junit.jupiter:junit-jupiter")
+
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+    // Source: https://mvnrepository.com/artifact/com.github.ben-manes/gradle-versions-plugin
+    runtimeOnly("com.github.ben-manes:gradle-versions-plugin:0.11.1")
+
+    // Source: https://mvnrepository.com/artifact/org.apache.commons/commons-lang3
+    implementation("org.apache.commons:commons-lang3:3.20.0")
+
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+checkstyle {
+    toolVersion = "10.12.4"
+}
+
+tasks.getByName("run", JavaExec::class) {
+    standardInput = System.`in`
+}
+
+tasks.jacocoTestReport {
+    dependsOn(tasks.test)
+    reports {
+        xml.required = true
+    }
+}
