eb4bd00c0a24d574ccba70c308a3b90e9d000557
NeoEmo <155008342+NeoEmo@users.noreply.github.com>
2026-07-01 15:27:29 +0300

    Create NumberSchemaTest.java

---
+++ app/src/test/java/hexlet/code/schemas/NumberSchemaTest.java
@@ -0,0 +1,35 @@
+package hexlet.code.schemas;
+
+import hexlet.code.Validator;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+public class NumberSchemaTest {
+
+    @Test
+    public void numberSchemaTest() {
+        var v = new Validator();
+        var schema = v.number();
+
+        assertTrue(schema.isValid(5));
+
+        assertTrue(schema.isValid(null));
+        assertTrue(schema.positive().isValid(null));
+
+        schema.required();
+
+        assertFalse(schema.isValid(null));
+        assertTrue(schema.isValid(10));
+
+        assertFalse(schema.isValid(-10));
+
+        schema.range(5, 10);
+
+        assertTrue(schema.isValid(5));
+        assertTrue(schema.isValid(10));
+        assertFalse(schema.isValid(4));
+        assertFalse(schema.isValid(11));
+    }
+}
