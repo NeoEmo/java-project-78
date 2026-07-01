0d815d94164efb3bd2e82096a9996190cd59b9ae
NeoEmo <155008342+NeoEmo@users.noreply.github.com>
2026-07-01 13:39:23 +0300

    add new test

--- app/src/test/java/hexlet/code/schemas/StringSchemaTest.java
+++ app/src/test/java/hexlet/code/schemas/StringSchemaTest.java
@@ -5,6 +5,7 @@ import hexlet.code.Validator;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 public class StringSchemaTest {
@@ -31,5 +32,16 @@ public class StringSchemaTest {
 
         var schema1 = v.string();
         assertTrue(schema1.minLength(10).minLength(4).isValid("Hexlet"));
+    }
+
+    @Test
+    public void stringSchemaTest2() {
+        var v = new Validator();
+        var schema = v.string();
+        var expected = "Validator";
+        var expected2 = "StringSchema";
+
+        assertEquals(expected, v.getClass().getSimpleName());
+        assertEquals(expected2, schema.getClass().getSimpleName());
     }
 }
