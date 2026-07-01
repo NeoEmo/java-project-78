4743aaff5e15bd5eba1ba23895b5da307153a741
NeoEmo <155008342+NeoEmo@users.noreply.github.com>
2026-07-01 15:26:54 +0300

    Create NumberSchema.java

---
+++ app/src/main/java/hexlet/code/schemas/NumberSchema.java
@@ -0,0 +1,45 @@
+package hexlet.code.schemas;
+
+public class NumberSchema implements BaseSchema<Integer> {
+    private boolean required;
+    private boolean positive;
+    private boolean range;
+    private Integer min;
+    private Integer max;
+
+    @Override
+    public boolean isValid(Integer integer) {
+        if (!required && integer == null) {
+            return true;
+        }
+
+        if (required && integer == null) {
+            return false;
+        }
+
+        if  (range && (integer < min || integer > max)) {
+            return false;
+        }
+
+        if (positive && integer <= 0) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public void required() {
+        this.required = true;
+    }
+
+    public NumberSchema positive() {
+        this.positive = true;
+        return this;
+    }
+
+    public void range(int min, int max) {
+        this.range = true;
+        this.min = min;
+        this.max = max;
+    }
+}
