f1a1a56ddf5b5db90e6af604981145f436eb7c66
NeoEmo <155008342+NeoEmo@users.noreply.github.com>
2026-07-04 17:15:48 +0300

    add MapSchema and test

---
+++ app/src/main/java/hexlet/code/schemas/MapSchema.java
@@ -0,0 +1,33 @@
+package hexlet.code.schemas;
+
+import java.util.Map;
+
+public class MapSchema implements BaseSchema<Map> {
+    private boolean required;
+    private boolean sizeOf;
+    private int size;
+
+    public void required() {
+        this.required = true;
+    }
+
+    public MapSchema sizeof(int size) {
+        this.sizeOf = true;
+        this.size = size;
+        return this;
+    }
+
+    @Override
+    public boolean isValid(Map map) {
+        if (!required && map == null) {
+            return true;
+        }
+        if (required && map == null) {
+            return false;
+        }
+        if (sizeOf && map.size() < size) {
+            return false;
+        }
+        return true;
+    }
+}
---
+++ app/src/test/java/hexlet/code/schemas/MapSchemaTest.java
@@ -0,0 +1,33 @@
+package hexlet.code.schemas;
+
+import hexlet.code.Validator;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+public class MapSchemaTest {
+
+    @Test
+    public void mapSchemaTest() {
+        var v = new Validator();
+        var schema = v.map();
+
+        assertTrue(schema.isValid(null));
+
+        schema.required();
+
+        assertFalse(schema.isValid(null));
+        assertTrue(schema.isValid(new HashMap<>()));
+        var data = new HashMap<String, String>();
+        data.put("key1", "value1");
+        assertTrue(schema.isValid(data));
+
+        schema.sizeof(2);
+        assertFalse(schema.isValid(data));
+        data.put("key2", "value2");
+        assertTrue(schema.isValid(data));
+    }
+}
