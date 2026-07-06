6bb396ff4ce29affdec3082a8d48e3ec434671e6
NeoEmo <155008342+NeoEmo@users.noreply.github.com>
2026-07-06 22:06:29 +0300

    refactor BaseSchema to abstract class

--- app/src/main/java/hexlet/code/schemas/BaseSchema.java
+++ app/src/main/java/hexlet/code/schemas/BaseSchema.java
@@ -1,5 +1,35 @@
 package hexlet.code.schemas;
 
-public interface BaseSchema<T> {
-    boolean isValid(T type);
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Predicate;
+
+public abstract class BaseSchema<T> {
+    protected Map<String, Predicate<T>> checks = new HashMap<>();
+    protected boolean required = false;
+
+    protected final void addCheck(String name, Predicate<T> validate) {
+        checks.put(name, validate);
+    }
+
+    public final boolean isValid(T value) {
+        if (required && value == null) {
+            return false;
+        }
+
+        if (!required && value == null) {
+            return true;
+        }
+
+        if (checks.isEmpty()) {
+            return true;
+        }
+
+        for (Map.Entry<String, Predicate<T>> entry : checks.entrySet()) {
+            if (!entry.getValue().test(value)) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
